4f85cc99b045e640de60e624e88fe1add5495e71
James Huynh <huynhshao@hotmail.com>
2024-02-02 20:34:29 -0500

    Finished basic api

--- src/main/java/dev/jameshuynh/movies/ReviewService.java
+++ src/main/java/dev/jameshuynh/movies/ReviewService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewService {
 	@Autowired
 	ReviewRepository reviewRepository;
