bc19c2e5e1d5ff4353263e6d40bb5a6114b4d7e0
James Huynh <huynh_james@hotmail.com>
2025-06-14 13:54:39 -0400

    Added a new optional endpoint for release dates

--- src/main/java/dev/jameshuynh/movies/MovieController.java
+++ src/main/java/dev/jameshuynh/movies/MovieController.java
@@ -3,28 +3,35 @@ package dev.jameshuynh.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
-	@Autowired
-	private MovieService movieService;
+    @Autowired
+    private MovieService movieService;
 
-	@GetMapping
-	public ResponseEntity<List<Movie>> getAllMovies() {
+	/*
+	@GetMapping({"/", "/{year}"})
+	public ResponseEntity<List<Movie>> getAllMovies(@PathVariable(required = false) String year) {
 		return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
 	}
+	*/
 
+    @GetMapping({"/", "/{releaseDate}"})
+    public ResponseEntity<List<Movie>> getAllMovies(@PathVariable Optional<String> optReleaseDate) {
 
-	@GetMapping("/{imdbId}")
-	public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
-		return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
-	}
+        List<Movie> movies = optReleaseDate.map(movieService::moviesByYear)
+                .orElseGet(movieService::allMovies);
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@RequestParam String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
 }
--- src/main/java/dev/jameshuynh/movies/MoviesApplication.java
+++ src/main/java/dev/jameshuynh/movies/MoviesApplication.java
@@ -4,21 +4,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 @RestController
 public class MoviesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		test();
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-
 	@GetMapping("/")
 	public ResponseEntity<String> apiRoot() {
-		return new ResponseEntity<String>("Hello, world!", HttpStatus.OK);
+		return new ResponseEntity<>("Hello, world!", HttpStatus.OK);
+	}
+
+
+	private static void test() {
+		final int test = 1;
+
+		// Exception handling
+		try {
+			System.out.println("throwing");
+			throw new SecurityException("TestException");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("always executed");
+		}
 	}
 
 }
